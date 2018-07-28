fdfaffa007c7effc9019c0abcba810378f987c3a
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2018-07-28 12:11:12 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRandomNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Random Numbers
+            // Define Data Types for Random Numbers
+            Random myRnd = new Random();
+
+            for (int i=1; i<=10; i++)
+            {
+                // Print Random Number for each Item Number
+                Console.WriteLine($"Item number {i} has the random number of {myRnd.Next(1, 100)}");
+            }
+        }
+    }
+}
